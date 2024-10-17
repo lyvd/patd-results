6eb80d779ade5499a9d6d9ee6a3bf16e4a0af491
Nguyen Thanh Cong <98400336+pakaremon@users.noreply.github.com>
2024-10-17 17:54:10 +0700

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,8 +1,1 @@
-import os
 
-directory = r'D:\HocTap\projectDrVuDucLy\final\patd-results\scan-results\bincapz\wolfi-apks'
-
-
-all_files = [f for f in os.listdir(directory) if f.endswith('.json')]
-print(len(all_files))
-print(len(set(all_files)))
